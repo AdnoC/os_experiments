a0619806ce5c200c1054d7f0f68b3c05c98286f2
AdnoC <adam.r.cutler@gmail.com>
2023-08-17 19:44:29 -0400

    up to timer

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,58 @@
 #![no_std]
 #![no_main]
 
+#![feature(abi_x86_interrupt)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
 
+mod serial;
+mod test_runner;
+
+mod vga_text;
+mod gdt;
+mod interrupts;
+
+// our existing panic handler
+#[cfg(not(test))] // new attribute
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
+
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    use test_runner::*;
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
 static HELLO: &[u8] = b"Hello World!";
+
+fn init() {
+    gdt::init();
+    interrupts::init_idt();
+    x86_64::instructions::interrupts::without_interrupts(|| {
+        unsafe { interrupts::PICS.lock().initialize() };
+    });
+    x86_64::instructions::interrupts::enable();
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    init();
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello world!!!!");
+
+    // #[cfg(test)]
+    // test_main();
 
     loop {}
 }
