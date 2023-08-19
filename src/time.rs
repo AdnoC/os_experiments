1aab3350695716ed5dad9c3b3b5c24675c5ca7a6
AdnoC <adam.r.cutler@gmail.com>
2023-08-19 12:40:16 -0400

    made it nicer

--- src/time.rs
+++ src/time.rs
@@ -16,26 +16,20 @@ fn timer_count() -> u64 {
     count
 }
 
+// TODO: convert to macro with ASM so that it is exact # of cycles
 pub fn wait_cycle(mut num: usize) {
     while num > 0 {
         num -= 1;
         unsafe { asm!("noop") };
     }
 }
-pub fn wait_microsec(msec: usize) {
+pub fn wait_microsec(msec: u64) {
     let freq = timer_frequency();
-    let dt = ((freq as usize / 1000) * msec) / 1000;
-    let then = timer_count() as usize;
+    let dt = ((freq as u64 / 1000) * msec) / 1000;
+    let then = timer_count();
     println!("timer freq = {}, dt = {}, then = {}, target = {}", freq, dt, then, then + dt);
-    let mut now = then;
-    let target = now.saturating_add(dt);
-    while now < target {
-        let count;
-        unsafe {
-            asm!("mrs {}, cntpct_el0", out(reg) count);
-        }
-        now = count;
-    }
+    let target = then.saturating_add(dt) as u64;
+    while timer_count() < target { }
     println!("done waiting");
     println!("now = {}", timer_count());
 }
